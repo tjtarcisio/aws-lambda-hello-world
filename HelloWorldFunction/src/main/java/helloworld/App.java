f38ba86d3371fb7cee90fb2403f1dd323f4d4a8f
Luan <luan.ribeiro@estudante.ifgoiano.edu.br>
2023-10-30 19:03:47 -0300

    nova implementacao

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -1,13 +1,5 @@
 package helloworld;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
@@ -18,7 +10,7 @@ import software.amazon.lambda.powertools.logging.Logging;
 import software.amazon.lambda.powertools.metrics.Metrics;
 import software.amazon.lambda.powertools.tracing.Tracing;
 
-import static software.amazon.lambda.powertools.tracing.CaptureMode.*;
+import static software.amazon.lambda.powertools.tracing.CaptureMode.DISABLED;
 
 /**
  * Handler for requests to Lambda function.
@@ -30,32 +22,14 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
     @Tracing(captureMode = DISABLED)
     @Metrics(captureColdStart = true)
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
-        var headers = new HashMap<String, String>();
-        headers.put("Content-Type", "application/json");
-        headers.put("X-Custom-Header", "application/json");
+        log.info("isso ai, amigao");
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        String output = """
+                { "message": "new hello world, testandooo", "location": "%s" }
+                """;
 
-        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
-                .withHeaders(headers);
-        try {
-            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            String output = String.format("""
-                    { "message": "hello world", "location": "%s" }""", pageContents);
-
-            return response
-                    .withStatusCode(200)
-                    .withBody(output);
-        } catch (IOException e) {
-            return response
-                    .withBody("{}")
-                    .withStatusCode(500);
-        }
-    }
-    @Tracing(namespace = "getPageContents")
-    private String getPageContents(String address) throws IOException {
-        log.info("Retrieving {}", address);
-        var url = new URL(address);
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
-            return br.lines().collect(Collectors.joining(System.lineSeparator()));
-        }
+        return response
+                .withStatusCode(200)
+                .withBody(output);
     }
 }
